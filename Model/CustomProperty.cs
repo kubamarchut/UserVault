3a91c49f1dc35ccaf03b08e2ecf7092118c6fc56
Kuba Marchut <kuba.marchut@gmail.com>
2025-10-28 12:48:03 +0100

    adding db schema + adjsuting properties of CustomProperty object to match db

--- Model/CustomProperty.cs
+++ Model/CustomProperty.cs
@@ -2,6 +2,8 @@
 {
     public class CustomProperty
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
         public required string Name { get; set; }
         public required string Value { get; set; }
     }
