e72ffb061cb6cf21000d82d673e2391f010007c4
Kuba Marchut <kuba.marchut@gmail.com>
2025-10-28 15:22:42 +0100

    adding swagger for easy REST API debuging

--- Program.cs
+++ Program.cs
@@ -1,31 +1,32 @@
-using System;
-using System.Linq;
-using UserVault.Model;
 using UserVault.Repositories;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = "Server=localhost;Database=UserVaultDb;User Id=sa;Password=mssqlP@ss;TrustServerCertificate=True;";
+
+builder.Services.AddSingleton<UserRepository>(new UserRepository(connectionString));
+
+builder.Services.AddControllers();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var swagger = builder.Build();
+
+if (swagger.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
+    swagger.UseSwagger();
+    swagger.UseSwaggerUI(c =>
     {
-        string connectionString = "Server=localhost;Database=UserVaultDb;User Id=sa;Password=mssqlP@ss;TrustServerCertificate=True;";
-
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
-
-        // Create an instance of UserRepository
-        var userRepository = new UserRepository(connectionString);
-
-        // Fetch all users from the database
-
-        // Map the root endpoint to print user info
-        app.MapGet("/", () =>
-        {
-            var users = userRepository.GetAllUsers();
-            return string.Join("\n\n", users.Select(user =>
-                $"ID: {user.Id}, Name: {user.Firstname} {user.Lastname}, BirthDate: {user.DateOfBirth.ToShortDateString()}, Sex: {user.Sex}, CustomProperties: {user.ShowCustomProperties()}"));
-        });
-
-        // Run the application
-        app.Run();
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserVault API v1");
+        c.RoutePrefix = string.Empty;
     }
+    );
 }
+
+swagger.UseHttpsRedirection();
+swagger.UseAuthorization();
+swagger.MapControllers();
+
+swagger.Run();
