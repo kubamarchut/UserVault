566d80dd5d9fb0a73150acc6bd4f40fb9c72cd30
Kuba Marchut <kuba.marchut@gmail.com>
2025-11-03 09:21:25 +0100

    adding backend validation for customProperties

--- backend/Dtos/CustomPropertyDto.cs
+++ backend/Dtos/CustomPropertyDto.cs
@@ -1,15 +1,26 @@
-﻿namespace UserVault.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserVault.Dtos
 {
     public class CustomPropertyDto
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Value { get; set; }
         public CustomPropertyDto(int id, string name, string value)
         {
             Id = id;
             Name = name;
             Value = value;
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
         }
     }
 }
